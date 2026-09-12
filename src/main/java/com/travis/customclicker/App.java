ef19dc156a9c9b7133001c99b4a28d5da9e005b4
Travis Morin <155117879+1999Travis250@users.noreply.github.com>
2026-09-12 15:35:31 -0300

    Build basic CustomClicker interface

--- src/main/java/com/travis/customclicker/App.java
+++ src/main/java/com/travis/customclicker/App.java
@@ -4,21 +4,24 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-import java.io.IOException;
+import javafx.stage.StageStyle;
 
 public class App extends Application {
 
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader loader = new FXMLLoader(
-                App.class.getResource("/views/main-view.fxml")
+    public void start(Stage stage) throws Exception {
+        Scene scene = new Scene(
+                FXMLLoader.load(
+                        App.class.getResource("/views/main-view.fxml")
+                ),
+                1200,
+                800
         );
 
-        Scene scene = new Scene(loader.load());
-
-        stage.setTitle("CustomClicker");
+        stage.initStyle(StageStyle.UNDECORATED);
         stage.setScene(scene);
+        stage.setResizable(false);
+        stage.centerOnScreen();
         stage.show();
     }
 
--- src/main/java/com/travis/customclicker/controller/MainController.java
+++ src/main/java/com/travis/customclicker/controller/MainController.java
@@ -1,15 +1,82 @@
 package com.travis.customclicker.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+import javafx.scene.Node;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
 public class MainController {
 
+    @FXML private Spinner<Integer> intervalSpinner;
+    @FXML private Spinner<Integer> xSpinner;
+    @FXML private Spinner<Integer> ySpinner;
+
+    @FXML private ComboBox<String> mouseButtonCombo;
+    @FXML private ComboBox<String> clickTypeCombo;
+    @FXML private ComboBox<String> repeatCombo;
+    @FXML private ComboBox<String> profileCombo;
+
+    private double xOffset;
+    private double yOffset;
+
     @FXML
-    private Button startButton;
+    private void initialize() {
+        intervalSpinner.setValueFactory(
+                new SpinnerValueFactory.IntegerSpinnerValueFactory(1, 60000, 100)
+        );
+
+        xSpinner.setValueFactory(
+                new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 10000, 0)
+        );
+
+        ySpinner.setValueFactory(
+                new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 10000, 0)
+        );
+
+        mouseButtonCombo.getItems().addAll("Left", "Right", "Middle");
+        mouseButtonCombo.setValue("Left");
+
+        clickTypeCombo.getItems().addAll("Single", "Double");
+        clickTypeCombo.setValue("Single");
+
+        repeatCombo.getItems().addAll("Until stopped", "Fixed amount");
+        repeatCombo.setValue("Until stopped");
+
+        profileCombo.getItems().add("Default");
+        profileCombo.setValue("Default");
+    }
 
     @FXML
     private void handleStart() {
         System.out.println("Start button clicked");
+    }
+
+    @FXML
+    private void handleMinimize(ActionEvent event) {
+        getStage(event).setIconified(true);
+    }
+
+    @FXML
+    private void handleClose(ActionEvent event) {
+        getStage(event).close();
+    }
+
+    @FXML
+    private void handleWindowPressed(MouseEvent event) {
+        xOffset = event.getSceneX();
+        yOffset = event.getSceneY();
+    }
+
+    @FXML
+    private void handleWindowDragged(MouseEvent event) {
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setX(event.getScreenX() - xOffset);
+        stage.setY(event.getScreenY() - yOffset);
+    }
+
+    private Stage getStage(ActionEvent event) {
+        return (Stage) ((Node) event.getSource()).getScene().getWindow();
     }
 }
